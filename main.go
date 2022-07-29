69abbe1ec31d351f1da41063baa45e9445124aeb
SBGoods <github@simplebox.anonaddy.com>
2022-07-29 10:19:38 -0400

    Simplify muxing

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"flag"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5/tf5server"
-	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 	"github.com/hashicorp/terraform-plugin-mux/tf5muxserver"
 	"github.com/hashicorp/terraform-provider-time/internal/provider"
 	"log"
@@ -13,7 +12,6 @@ import (
 	"github.com/hashicorp/terraform-provider-time/internal/tftime"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-	"github.com/hashicorp/terraform-plugin-mux/tf6to5server"
 )
 
 // Generate the Terraform provider documentation using `tfplugindocs`:
@@ -21,25 +19,14 @@ import (
 
 func main() {
 	ctx := context.Background()
+
 	var debug bool
 
 	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
 
-	downgradedProvider, err := tf6to5server.DowngradeServer(
-		ctx,
-		func() tfprotov6.ProviderServer {
-			return providerserver.NewProtocol6(provider.New())()
-		},
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	providers := []func() tfprotov5.ProviderServer{
-		func() tfprotov5.ProviderServer {
-			return downgradedProvider
-		},
+		providerserver.NewProtocol5(provider.New()),
 		tftime.Provider().GRPCProvider,
 	}
 
