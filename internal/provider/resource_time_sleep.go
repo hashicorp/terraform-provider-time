9094523799692ef47eb35ca0b93e2fdb5a25f33f
SBGoods <github@simplebox.anonaddy.com>
2022-09-13 15:18:14 -0400

    implement newResource and CRUD operations for time_sleep resource

--- internal/provider/resource_time_sleep.go
+++ internal/provider/resource_time_sleep.go
@@ -2,7 +2,10 @@ package provider
 
 import (
 	"context"
+	"fmt"
 	"regexp"
+	"strings"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-framework-validators/schemavalidator"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
@@ -64,8 +67,7 @@ func (t timeSleepResourceType) GetSchema(ctx context.Context) (tfsdk.Schema, dia
 }
 
 func (t timeSleepResourceType) NewResource(ctx context.Context, p tfsdk.Provider) (tfsdk.Resource, diag.Diagnostics) {
-	//TODO implement me
-	panic("implement me")
+	return &timeSleepResource{}, nil
 }
 
 var (
@@ -76,27 +78,152 @@ var (
 type timeSleepResource struct {
 }
 
-func (t timeSleepResource) ImportState(ctx context.Context, request tfsdk.ImportResourceStateRequest, response *tfsdk.ImportResourceStateResponse) {
-	//TODO implement me
-	panic("implement me")
+type timeSleepModelV0 struct {
+	CreateDuration  types.String `tfsdk:"create_duration"`
+	DestroyDuration types.String `tfsdk:"destroy_duration"`
+	Triggers        types.Map    `tfsdk:"triggers"`
+	ID              types.String `tfsdk:"id"`
 }
 
-func (t timeSleepResource) Create(ctx context.Context, request tfsdk.CreateResourceRequest, response *tfsdk.CreateResourceResponse) {
-	//TODO implement me
-	panic("implement me")
+func (t timeSleepResource) ImportState(ctx context.Context, req tfsdk.ImportResourceStateRequest, resp *tfsdk.ImportResourceStateResponse) {
+	id := req.ID
+
+	idParts := strings.Split(id, ",")
+
+	if len(idParts) != 2 || (idParts[0] == "" && idParts[1] == "") {
+		resp.Diagnostics.AddError(
+			"Unexpected Format of ID",
+			fmt.Sprintf("Unexpected format of ID (%q), expected CREATEDURATION,DESTROYDURATION where at least one value is non-empty", id))
+
+		return
+	}
+
+	state := timeSleepModelV0{
+		CreateDuration:  types.String{Null: true},
+		DestroyDuration: types.String{Null: true},
+		ID:              types.String{Value: time.Now().UTC().Format(time.RFC3339)},
+	}
+
+	if idParts[0] != "" {
+		_, err := time.ParseDuration(idParts[0])
+		if err != nil {
+			resp.Diagnostics.AddError(
+				"Import time sleep error",
+				"The create_duration cannot be parsed\n\n+"+
+					fmt.Sprintf("Original Error: %s", err),
+			)
+			return
+		}
+		state.CreateDuration.Null = false
+		state.CreateDuration.Value = idParts[0]
+	}
+
+	if idParts[1] != "" {
+		_, err := time.ParseDuration(idParts[1])
+		if err != nil {
+			resp.Diagnostics.AddError(
+				"Import time sleep error",
+				"The create_duration cannot be parsed\n\n+"+
+					fmt.Sprintf("Original Error: %s", err),
+			)
+			return
+		}
+		state.DestroyDuration.Null = false
+		state.DestroyDuration.Value = idParts[1]
+	}
+
+	state.Triggers.ElemType = types.StringType
+	diags := resp.State.Set(ctx, state)
+	resp.Diagnostics.Append(diags...)
+
 }
 
-func (t timeSleepResource) Read(ctx context.Context, request tfsdk.ReadResourceRequest, response *tfsdk.ReadResourceResponse) {
-	//TODO implement me
-	panic("implement me")
+func (t timeSleepResource) Create(ctx context.Context, req tfsdk.CreateResourceRequest, resp *tfsdk.CreateResourceResponse) {
+	var plan timeSleepModelV0
+
+	diags := req.Plan.Get(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	if plan.CreateDuration.Value != "" {
+		duration, err := time.ParseDuration(plan.CreateDuration.Value)
+		if err != nil {
+			resp.Diagnostics.AddError(
+				"Create time sleep error",
+				"The create_duration cannot be parsed\n\n+"+
+					fmt.Sprintf("Original Error: %s", err),
+			)
+			return
+		}
+
+		select {
+		case <-ctx.Done():
+			resp.Diagnostics.AddError(
+				"Create time sleep error",
+				fmt.Sprintf("Original Error: %s", ctx.Err()),
+			)
+			return
+		case <-time.After(duration):
+		}
+	}
+
+	state := timeSleepModelV0{
+		CreateDuration:  plan.CreateDuration,
+		DestroyDuration: plan.DestroyDuration,
+		Triggers:        plan.Triggers,
+		ID:              types.String{Value: time.Now().UTC().Format(time.RFC3339)},
+	}
+	diags = resp.State.Set(ctx, state)
+	resp.Diagnostics.Append(diags...)
+}
+
+func (t timeSleepResource) Read(ctx context.Context, req tfsdk.ReadResourceRequest, resp *tfsdk.ReadResourceResponse) {
+
 }
 
 func (t timeSleepResource) Update(ctx context.Context, request tfsdk.UpdateResourceRequest, response *tfsdk.UpdateResourceResponse) {
-	//TODO implement me
-	panic("implement me")
+
 }
 
-func (t timeSleepResource) Delete(ctx context.Context, request tfsdk.DeleteResourceRequest, response *tfsdk.DeleteResourceResponse) {
-	//TODO implement me
-	panic("implement me")
+func (t timeSleepResource) Delete(ctx context.Context, req tfsdk.DeleteResourceRequest, resp *tfsdk.DeleteResourceResponse) {
+	var state timeSleepModelV0
+
+	diags := req.State.Get(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	if state.DestroyDuration.Value != "" {
+		duration, err := time.ParseDuration(state.DestroyDuration.Value)
+		if err != nil {
+			resp.Diagnostics.AddError(
+				"Delete time sleep error",
+				"The create_duration cannot be parsed\n\n+"+
+					fmt.Sprintf("Original Error: %s", err),
+			)
+			return
+		}
+
+		select {
+		case <-ctx.Done():
+			resp.Diagnostics.AddError(
+				"Delete time sleep error",
+				fmt.Sprintf("Original Error: %s", ctx.Err()),
+			)
+			return
+		case <-time.After(duration):
+		}
+	}
+
+	state = timeSleepModelV0{
+		CreateDuration:  types.String{Null: true},
+		DestroyDuration: types.String{Null: true},
+		ID:              types.String{Null: true},
+	}
+	state.Triggers.ElemType = types.StringType
+	diags = resp.State.Set(ctx, state)
+	resp.Diagnostics.Append(diags...)
 }
