76339ec997cc6ef3a426c367c323dd1ca6c197fc
SBGoods <github@simplebox.anonaddy.com>
2022-09-14 16:08:12 -0400

    Implement schema for time_static resource

--- internal/provider/resource_time_static.go
+++ internal/provider/resource_time_static.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-provider-time/internal/validators/timevalidator"
 )
 
 var _ tfsdk.ResourceType = (*timeStaticResourceType)(nil)
@@ -11,13 +13,83 @@ var _ tfsdk.ResourceType = (*timeStaticResourceType)(nil)
 type timeStaticResourceType struct{}
 
 func (t timeStaticResourceType) GetSchema(ctx context.Context) (tfsdk.Schema, diag.Diagnostics) {
-	//TODO implement me
-	panic("implement me")
+	return tfsdk.Schema{
+		Attributes: map[string]tfsdk.Attribute{
+			"day": {
+				Description: "Number day of timestamp.",
+				Type:        types.Int64Type,
+				Computed:    true,
+			},
+			"hour": {
+				Description: "Number hour of timestamp.",
+				Type:        types.Int64Type,
+				Computed:    true,
+			},
+			"triggers": {
+				Description: "Arbitrary map of values that, when changed, will trigger a new base timestamp value to be saved. " +
+					"See [the main provider documentation](../index.md) for more information.",
+				Type: types.MapType{
+					ElemType: types.StringType,
+				},
+				Optional: true,
+				PlanModifiers: []tfsdk.AttributePlanModifier{
+					tfsdk.RequiresReplace(),
+				},
+			},
+			"minute": {
+				Description: "Number minute of timestamp.",
+				Type:        types.Int64Type,
+				Computed:    true,
+			},
+			"month": {
+				Description: "Number month of timestamp.",
+				Type:        types.Int64Type,
+				Computed:    true,
+			},
+			"rfc3339": {
+				Description: "Base timestamp in " +
+					"[RFC3339](https://datatracker.ietf.org/doc/html/rfc3339#section-5.8) format " +
+					"(see [RFC3339 time string](https://tools.ietf.org/html/rfc3339#section-5.8) e.g., " +
+					"`YYYY-MM-DDTHH:MM:SSZ`). Defaults to the current time.",
+				Type:     types.StringType,
+				Optional: true,
+				Computed: true,
+				PlanModifiers: []tfsdk.AttributePlanModifier{
+					tfsdk.RequiresReplace(),
+				},
+				Validators: []tfsdk.AttributeValidator{
+					timevalidator.IsRFC3339Time(),
+				},
+			},
+			"second": {
+				Description: "Number second of timestamp.",
+				Type:        types.Int64Type,
+				Computed:    true,
+			},
+			"unix": {
+				Description: "Number of seconds since epoch time, e.g. `1581489373`.",
+				Type:        types.Int64Type,
+				Computed:    true,
+			},
+			"year": {
+				Description: "Number year of timestamp.",
+				Type:        types.Int64Type,
+				Computed:    true,
+			},
+			"id": {
+				Description: "RFC3339 format of the offset timestamp, e.g. `2020-02-12T06:36:13Z`.",
+				Type:        types.StringType,
+				Computed:    true,
+				PlanModifiers: []tfsdk.AttributePlanModifier{
+					tfsdk.UseStateForUnknown(),
+				},
+			},
+		},
+	}, nil
 }
 
 func (t timeStaticResourceType) NewResource(ctx context.Context, p tfsdk.Provider) (tfsdk.Resource, diag.Diagnostics) {
-	//TODO implement me
-	panic("implement me")
+	return &timeStaticResource{}, nil
 }
 
 var (
