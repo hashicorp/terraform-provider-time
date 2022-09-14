b79d000d8312f1e2c5ec750b7c71a240bd1ebcb7
SBGoods <github@simplebox.anonaddy.com>
2022-09-14 17:20:34 -0400

    Implement CRUD operations for time_static resource

--- internal/provider/resource_time_static.go
+++ internal/provider/resource_time_static.go
@@ -2,11 +2,13 @@ package provider
 
 import (
 	"context"
+	"fmt"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-provider-time/internal/validators/timevalidator"
+	"time"
 )
 
 var _ tfsdk.ResourceType = (*timeStaticResourceType)(nil)
@@ -105,22 +107,76 @@ func (t timeStaticResource) ImportState(ctx context.Context, req tfsdk.ImportRes
 	tfsdk.ResourceImportStatePassthroughID(ctx, path.Root("id"), req, resp)
 }
 
-func (t timeStaticResource) Create(ctx context.Context, request tfsdk.CreateResourceRequest, response *tfsdk.CreateResourceResponse) {
-	//TODO implement me
-	panic("implement me")
+type timeStaticModelV0 struct {
+	Day      types.Int64  `tfsdk:"day"`
+	Hour     types.Int64  `tfsdk:"hour"`
+	Triggers types.Map    `tfsdk:"triggers"`
+	Minute   types.Int64  `tfsdk:"minute"`
+	Month    types.Int64  `tfsdk:"month"`
+	RFC3339  types.String `tfsdk:"rfc3339"`
+	Second   types.Int64  `tfsdk:"second"`
+	Unix     types.Int64  `tfsdk:"unix"`
+	Year     types.Int64  `tfsdk:"year"`
+	ID       types.String `tfsdk:"id"`
 }
 
-func (t timeStaticResource) Read(ctx context.Context, request tfsdk.ReadResourceRequest, response *tfsdk.ReadResourceResponse) {
-	//TODO implement me
-	panic("implement me")
+func (t timeStaticResource) Create(ctx context.Context, req tfsdk.CreateResourceRequest, resp *tfsdk.CreateResourceResponse) {
+	var plan timeStaticModelV0
+
+	diags := req.Plan.Get(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	timestamp := time.Now().UTC()
+
+	if plan.RFC3339.Value != "" {
+		var err error
+
+		if timestamp, err = time.Parse(time.RFC3339, plan.RFC3339.Value); err != nil {
+			resp.Diagnostics.AddError(
+				"Create time rotating error",
+				"The rfc3339 timestamp that was supplied could not be parsed as RFC3339.\n\n+"+
+					fmt.Sprintf("Original Error: %s", err),
+			)
+			return
+		}
+	}
+
+	formattedTimestamp := timestamp.Format(time.RFC3339)
+
+	state := timeStaticModelV0{
+		Triggers: plan.Triggers,
+		Year:     types.Int64{Value: int64(timestamp.Year())},
+		Month:    types.Int64{Value: int64(timestamp.Month())},
+		Day:      types.Int64{Value: int64(timestamp.Day())},
+		Hour:     types.Int64{Value: int64(timestamp.Hour())},
+		Minute:   types.Int64{Value: int64(timestamp.Minute())},
+		Second:   types.Int64{Value: int64(timestamp.Second())},
+		RFC3339:  types.String{Value: formattedTimestamp},
+		Unix:     types.Int64{Value: timestamp.Unix()},
+		ID:       types.String{Value: formattedTimestamp},
+	}
+
+	diags = resp.State.Set(ctx, state)
+	resp.Diagnostics.Append(diags...)
 }
 
-func (t timeStaticResource) Update(ctx context.Context, request tfsdk.UpdateResourceRequest, response *tfsdk.UpdateResourceResponse) {
-	//TODO implement me
-	panic("implement me")
+func (t timeStaticResource) Read(ctx context.Context, req tfsdk.ReadResourceRequest, resp *tfsdk.ReadResourceResponse) {
+
 }
 
-func (t timeStaticResource) Delete(ctx context.Context, request tfsdk.DeleteResourceRequest, response *tfsdk.DeleteResourceResponse) {
-	//TODO implement me
-	panic("implement me")
+func (t timeStaticResource) Update(ctx context.Context, req tfsdk.UpdateResourceRequest, resp *tfsdk.UpdateResourceResponse) {
+	var data timeStaticModelV0
+
+	// Read Terraform plan data into the model
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
+
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
+
+func (t timeStaticResource) Delete(ctx context.Context, req tfsdk.DeleteResourceRequest, resp *tfsdk.DeleteResourceResponse) {
+
 }
