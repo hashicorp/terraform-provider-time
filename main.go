a4d57f8a2f5fb74df4482d55328de9b3f039eaa4
SBGoods <github@simplebox.anonaddy.com>
2022-09-19 18:31:11 -0400

    Remove provider muxer

--- main.go
+++ main.go
@@ -3,13 +3,8 @@ package main
 import (
 	"context"
 	"flag"
-	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
-	"github.com/hashicorp/terraform-plugin-go/tfprotov5/tf5server"
-	"github.com/hashicorp/terraform-plugin-mux/tf5muxserver"
 	"github.com/hashicorp/terraform-provider-time/internal/provider"
 	"log"
-
-	"github.com/hashicorp/terraform-provider-time/internal/tftime"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 )
@@ -18,34 +13,16 @@ import (
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 
 func main() {
-	ctx := context.Background()
-
 	var debug bool
 
 	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
 
-	providers := []func() tfprotov5.ProviderServer{
-		providerserver.NewProtocol5(provider.New()),
-		tftime.Provider().GRPCProvider,
-	}
-
-	muxServer, err := tf5muxserver.NewMuxServer(ctx, providers...)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var serveOpts []tf5server.ServeOpt
-
-	if debug {
-		serveOpts = append(serveOpts, tf5server.WithManagedDebug())
-	}
-
-	err = tf5server.Serve(
-		"registry.terraform.io/hashicorp/time",
-		muxServer.ProviderServer,
-		serveOpts...,
-	)
+	err := providerserver.Serve(context.Background(), provider.New, providerserver.ServeOpts{
+		Address:         "registry.terraform.io/hashicorp/time",
+		Debug:           debug,
+		ProtocolVersion: 5,
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
