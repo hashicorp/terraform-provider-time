4a39945ade18a1c500a10e94e44f0e940d740afe
Christian Fosli <christian.fosli@capgemini.com>
2022-11-09 15:38:53 +0100

    docs: Fix misleading docs for time_rotating.id
    Currently the id of `time_rotating` resource is set to `formattedTimestamp`,
    not `formattedRotationTimestamp`, however the documentation suggests otherwise.
    
    This updates to docs to match the implementation.

--- internal/provider/resource_time_rotating.go
+++ internal/provider/resource_time_rotating.go
@@ -175,7 +175,7 @@ func (t timeRotatingResource) GetSchema(ctx context.Context) (tfsdk.Schema, diag
 				Computed:    true,
 			},
 			"id": {
-				Description: "RFC3339 format of the offset timestamp, e.g. `2020-02-12T06:36:13Z`.",
+				Description: "RFC3339 format of the timestamp, e.g. `2020-02-12T06:36:13Z`.",
 				Type:        types.StringType,
 				Computed:    true,
 			},
